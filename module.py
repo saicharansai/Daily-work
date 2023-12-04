f2cab8f63db8bb320dabdb126fb660c0e5fce158
saicharansai <110266648+saicharansai@users.noreply.github.com>
2023-12-04 15:13:41 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+import datetime
+
+t = datetime.datetime(2024, 4, 15, 20, 15)  
+
+print(t)
+print("year:", t.year)
+print("month:", t.month)
+print("day:", t.day)
+print("hour:", t.hour)
+print("minute:", t.minute)
+print("second:", t.second)
+
+
+# In[23]:
+
+
+import datetime
+
+today = datetime.date.today()
+
+print(today)
+print('ctime:', today.ctime())
+print('year:', today.year)
+print('month:', today.month)
+print("hour:", t.hour)
+print("minute:", t.minute)
+print("second:", t.second)
+
+
+# In[25]:
+
+
+import math
+print(math.pi)
+
+
+# In[28]:
+
+
+import math as m 
+print(m.pi)
+
+
+# In[34]:
+
+
+import math
+print(math.tan(45))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
